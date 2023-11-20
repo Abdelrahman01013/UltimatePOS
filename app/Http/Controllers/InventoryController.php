848c1c1f94863258a8a5523a0d1d29a0f6a9ac3c
aminekhaled <aminewafie@gmail.com>
2023-11-20 17:19:08 +0100

    create inventory part

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -1,11 +1,36 @@
 <?php
 
+
+
 namespace App\Http\Controllers;
 
+use App\BusinessLocation;
+use App\Contact;
+use App\Product;
+use App\System;
+use App\User;
+use App\Utils\ModuleUtil;
+use DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+use Yajra\DataTables\Facades\DataTables;
+use Spatie\Activitylog\Models\Activity;
+use Illuminate\Support\Facades\Auth;
+
+
 
 class InventoryController extends Controller
 {
+
+
+    public function __construct(ModuleUtil $moduleUtil)
+    {
+        $this->moduleUtil = $moduleUtil;
+    }
+
+
+
     /**
      * Display a listing of the resource.
      *
@@ -13,8 +38,42 @@ class InventoryController extends Controller
      */
     public function index()
     {
-        //
+        if (!auth()->user()->can('user.view') && !auth()->user()->can('user.create')) {
+            abort(403, 'Unauthorized action.');
+        }
+
+        if (request()->ajax()) {
+
+            $bussness_locations = BusinessLocation::all();
+
+            return Datatables::of($bussness_locations)
+                ->addColumn(
+                    'action',
+                    '@can("user.update")
+                        <a href="{{action(\'InventoryController@inventoryByLocation\', [$id])}}" class="btn btn-xs btn-primary"><i class="glyphicon glyphicon-edit"></i> @lang("lang_v1.inventory")</a>
+                        &nbsp;
+                    @endcan
+                    @can("user.view")
+                    <a href="{{action(\'ManageUserController@show\', [$id])}}" class="btn btn-xs btn-info"><i class="fa fa-eye"></i> @lang("messages.view")</a>
+                    &nbsp;
+                    @endcan
+                    @can("user.delete")
+                        <button data-href="{{action(\'ManageUserController@destroy\', [$id])}}" class="btn btn-xs btn-danger delete_user_button"><i class="glyphicon glyphicon-trash"></i> @lang("messages.delete")</button>
+                    @endcan'
+                )
+                ->removeColumn('id')
+                ->rawColumns(['action', 'name'])
+                ->make(true);
+        }
+
+        return view('inventories.index');
     }
+
+    public function inventoryByLocation($id) {
+        return view('inventories.inventory-by-location');
+    }
+
+
 
     /**
      * Show the form for creating a new resource.
---
+++ resources/views/inventories/inventory-by-location.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('title', __( 'lang_v1.inventory' ))
+
+@section('content')
+
+
+
+@stop
+
+@section('javascript')
+
+
+
+@endsection
