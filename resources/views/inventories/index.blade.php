7e619c91523c9344c07ffc76ab22ee6de077e167
Abdelrahman Ahmed <122418474+Abdelrahman01013@users.noreply.github.com>
2023-11-21 22:20:38 +0200

    add less And more

--- resources/views/inventories/index.blade.php
+++ resources/views/inventories/index.blade.php
@@ -1,107 +1,112 @@
 @extends('layouts.app')
-@section('title', __( 'lang_v1.inventory' ))
+@section('title', __('lang_v1.inventory'))
 
 @section('content')
 
-<!-- Content Header (Page header) -->
-<section class="content-header">
-    <h1>@lang( 'user.users' )
-        <small>@lang( 'user.manage_users' )</small>
-    </h1>
-    <!-- <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-        <li class="active">Here</li>
-    </ol> -->
-</section>
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>@lang('الجرد')
+            <small>@lang('جرد الفروع')</small>
+        </h1>
+        <!-- <ol class="breadcrumb">
+                    <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+                    <li class="active">Here</li>
+                </ol> -->
+    </section>
 
-<!-- Main content -->
-<section class="content">
-    @component('components.widget', ['class' => 'box-primary', 'title' => __( 'user.all_users' )])
-        @can('user.create')
-            @slot('tool')
-                <div class="box-tools">
-                    <a class="btn btn-block btn-primary" 
-                    href="{{action('ManageUserController@create')}}" >
-                    <i class="fa fa-plus"></i> @lang( 'messages.add' )</a>
-                 </div>
-            @endslot
-        @endcan
-        @can('user.view')
-            <div class="table-responsive">
-                <table class="table table-bordered table-striped" id="inventory_table">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Country</th>
-                            <th>State</th>
-                            <th>City</th>
-                            <th>@lang( 'messages.action' )</th>
-                        </tr>
-                    </thead>
-                </table>
-            </div>
-        @endcan
-    @endcomponent
+    <!-- Main content -->
+    <section class="content">
+        @component('components.widget', ['class' => 'box-primary', 'title' => __('')])
+            @can('user.create')
+                @slot('tool')
+                    <div class="box-tools">
+                        <a class="btn btn-block btn-primary" href="{{ action('ManageUserController@create') }}">
+                            <i class="fa fa-plus"></i> @lang('messages.add')</a>
+                    </div>
+                @endslot
+            @endcan
+            @can('user.view')
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped" id="inventory_table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Country</th>
+                                <th>State</th>
+                                <th>City</th>
+                                <th>@lang('messages.action')</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            @endcan
+        @endcomponent
 
-    <div class="modal fade user_modal" tabindex="-1" role="dialog" 
-    	aria-labelledby="gridSystemModalLabel">
-    </div>
+        <div class="modal fade user_modal" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
+        </div>
 
-</section>
-<!-- /.content -->
+    </section>
+    <!-- /.content -->
 @stop
 @section('javascript')
-<script type="text/javascript">
-    //Roles table
-    $(document).ready( function(){
-        var inventory_table = $('#inventory_table').DataTable({
-            processing: true,
-            serverSide: true,
-            ajax: '/inventory',
-            columnDefs: [ {
-                "targets": [4],
-                "orderable": false,
-                "searchable": false
-            } ],
-            "columns":[
-                {"data":"name"},
-                {"data":"country"},
-                {"data":"state"},
-                {"data":"city"},
-                {"data":"action"}
-            ]
-        });
-        $(document).on('click', 'button.delete_user_button', function(){
-            swal({
-              title: LANG.sure,
-              text: LANG.confirm_delete_user,
-              icon: "warning",
-              buttons: true,
-              dangerMode: true,
-            }).then((willDelete) => {
-                if (willDelete) {
-                    var href = $(this).data('href');
-                    var data = $(this).serialize();
-                    $.ajax({
-                        method: "DELETE",
-                        url: href,
-                        dataType: "json",
-                        data: data,
-                        success: function(result){
-                            if(result.success == true){
-                                toastr.success(result.msg);
-                                users_table.ajax.reload();
-                            } else {
-                                toastr.error(result.msg);
+    <script type="text/javascript">
+        //Roles table
+        $(document).ready(function() {
+            var inventory_table = $('#inventory_table').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: '/inventory',
+                columnDefs: [{
+                    "targets": [4],
+                    "orderable": false,
+                    "searchable": false
+                }],
+                "columns": [{
+                        "data": "name"
+                    },
+                    {
+                        "data": "country"
+                    },
+                    {
+                        "data": "state"
+                    },
+                    {
+                        "data": "city"
+                    },
+                    {
+                        "data": "action"
+                    }
+                ]
+            });
+            $(document).on('click', 'button.delete_user_button', function() {
+                swal({
+                    title: LANG.sure,
+                    text: LANG.confirm_delete_user,
+                    icon: "warning",
+                    buttons: true,
+                    dangerMode: true,
+                }).then((willDelete) => {
+                    if (willDelete) {
+                        var href = $(this).data('href');
+                        var data = $(this).serialize();
+                        $.ajax({
+                            method: "DELETE",
+                            url: href,
+                            dataType: "json",
+                            data: data,
+                            success: function(result) {
+                                if (result.success == true) {
+                                    toastr.success(result.msg);
+                                    users_table.ajax.reload();
+                                } else {
+                                    toastr.error(result.msg);
+                                }
                             }
-                        }
-                    });
-                }
-             });
+                        });
+                    }
+                });
+            });
+
         });
-        
-    });
-    
-    
-</script>
+    </script>
 @endsection
