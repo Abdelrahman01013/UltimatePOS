c2e5593c0cd19056bcb8026b112c68815bda91b3
Abdelrahman Ahmed <122418474+Abdelrahman01013@users.noreply.github.com>
2023-11-22 02:05:25 +0200

    add database

---
+++ database/migrations/2023_11_22_050123_create_inventries_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInventriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+
+            $table->unsignedInteger('product_id');
+            $table->unsignedInteger('location_id');
+            $table->integer('dif');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('location_id')->references('id')->on('business_locations')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventries');
+    }
+}
